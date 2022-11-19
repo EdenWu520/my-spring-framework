2ca1dfeca2dcbb80b2faa8f74d0a01ec2fe60c77
Eden Wu <27047620@qq.com>
2022-11-19 21:07:26 +0800

    加入模板模式的相关注释

--- src/main/java/site/leiwa/springframework/beans/support/AbstractBeanFactory.java
+++ src/main/java/site/leiwa/springframework/beans/support/AbstractBeanFactory.java
@@ -9,9 +9,11 @@ import site.leiwa.springframework.beans.BeanFactory;
  * @desc: todo
  * @since 2022/11/19
  */
+// 全称是模板方法设计模式。模板模式主要是用来解决复用和扩展两个问题
+// https://time.geekbang.org/column/article/212049
 public abstract class AbstractBeanFactory extends DefaultSingletonBeanRegistry implements BeanFactory {
     @Override
-    public Object getBean(String name) throws BeansException { // 设计模式，模板方法
+    public Object getBean(String name) throws BeansException {
         Object bean = getSingleton(name);
         if (bean != null) {
             return bean;
